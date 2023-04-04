40a731b3174e88700f46b44c86ef3507887df4a4
khuseyn-khasiev <khuseyn-khasiev@mail.ru>
2023-04-04 15:38:24 +0300

    добавили middleware cors

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,8 @@ const {
 // eslint-disable-next-line import/no-extraneous-dependencies
 const cookieParser = require('cookie-parser');
 // eslint-disable-next-line import/no-extraneous-dependencies
-const cors = require('cors');
+// const cors = require('cors');
+const handleCors = require('./middlewares/cors');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { ERROR_NOT_FOUND } = require('./errors');
@@ -21,7 +22,10 @@ const { errorLogger, requestWinston } = require('./middlewares/Logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(handleCors);
+/*
 app.use(cors({ origin: ['https://mestogram.nomoredomains.monster'], credentials: true }));
+*/
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
